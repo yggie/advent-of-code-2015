c142067ad6bdd4c5904d09dc41f0104207196043
Bryan Yap <bryan.yap@alliants.com>
2015-12-15 14:57:26 +0000

    day 4 part 1

---
+++ day_4.1.exs
@@ -0,0 +1,47 @@
+input = case System.argv do
+  ["--file", filename] -> File.read!(filename)
+  [input] -> input
+  _ -> raise "Invalid input"
+end
+
+defmodule InfiniteSequence do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def init(:ok) do
+    {:ok, 1}
+  end
+
+  def handle_call(:next_number, _from, number) do
+    {:reply, number, number + 1}
+  end
+end
+
+{:ok, _pid} = InfiniteSequence.start_link
+
+check_md5 = fn
+  ("00000" <> _rest) ->
+    true
+
+  (_anything_else) ->
+    false
+end
+
+compute_md5 = fn(string) ->
+  :crypto.hash(:md5, input <> string) |> Base.encode16(case: :lower)
+end
+
+check_value = fn(value) ->
+  value |> to_string |> compute_md5.() |> check_md5.()
+end
+
+answer = Stream.repeatedly(&InfiniteSequence.next_number/0) |> Enum.find(&check_value.(&1))
+
+IO.puts(answer)
