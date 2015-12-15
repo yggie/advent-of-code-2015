91800a2f66ec2c00bbdb3d403749a53a9dec57a3
Bryan Yap <bryan.yap@alliants.com>
2015-12-15 13:04:24 +0000

    day 3 part 2

---
+++ day_3.2.exs
@@ -0,0 +1,27 @@
+input = case System.argv do
+  ["--file", filename] -> File.read!(filename)
+  [input] -> input
+  _ -> raise "Invalid input"
+end
+
+{_, map} = input
+  |> String.to_char_list
+  |> Enum.reduce({{{0, 0}, {0, 0}}, %{ {0, 0} => 2 }}, fn(char, {{{x, y}, next_coord}, map}) ->
+    coord = case char do
+      ?> -> {x + 1, y}
+      ?^ -> {x, y + 1}
+      ?< -> {x - 1, y}
+      ?v -> {x, y - 1}
+      _ -> nil
+    end
+
+    if coord == nil do
+      {{next_coord, {x, y}}, map}
+    else
+      {{next_coord, coord}, Map.put(map, coord, Map.get(map, coord, 0))}
+    end
+  end)
+
+answer = map |> Map.keys |> Enum.count
+
+IO.puts(answer)
