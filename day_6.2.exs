9a96358009f3525455f56c5ca9b9d1f1898a9704
Bryan Yap <bryan.yap@alliants.com>
2015-12-17 17:30:22 +0000

    day 6 part 2

---
+++ day_6.2.exs
@@ -0,0 +1,65 @@
+input = case System.argv do
+  ["--file", filename] -> File.read!(filename)
+  [input] -> input
+  _ -> raise "Invalid input"
+end
+
+defmodule LightController do
+  def perform_operation("turn on " <> range_string, light_grid) do
+    turn_on(light_grid, parse_range(range_string))
+  end
+
+  def perform_operation("turn off " <> range_string, light_grid) do
+    turn_off(light_grid, parse_range(range_string))
+  end
+
+  def perform_operation("toggle " <> range_string, light_grid) do
+    toggle(light_grid, parse_range(range_string))
+  end
+
+  def perform_operation("", light_grid) do
+    light_grid
+  end
+
+  defp parse_range(command) do
+    matches = Regex.scan(~r/^(\d+),(\d+) through (\d+),(\d+)$/, command)
+    [[_string, string_start_x, string_start_y, string_end_x, string_end_y]] = matches
+    {start_x, ""} = Integer.parse(string_start_x)
+    {start_y, ""} = Integer.parse(string_start_y)
+    {end_x, ""} = Integer.parse(string_end_x)
+    {end_y, ""} = Integer.parse(string_end_y)
+
+    for i <- start_x..end_x, j <- start_y..end_y do
+      i + 1000*j
+    end
+  end
+
+  defp turn_on(light_grid, range) do
+    Enum.reduce(range, light_grid, fn(position, light_grid) ->
+      brightness = Map.get(light_grid, position, 0)
+      Map.put(light_grid, position, brightness + 1)
+    end)
+  end
+
+  defp turn_off(light_grid, range) do
+    Enum.reduce(range, light_grid, fn(position, light_grid) ->
+      brightness = Map.get(light_grid, position, 0)
+      Map.put(light_grid, position, max(brightness - 1, 0))
+    end)
+  end
+
+  defp toggle(light_grid, range) do
+    Enum.reduce(range, light_grid, fn(position, light_grid) ->
+      brightness = Map.get(light_grid, position, 0)
+      Map.put(light_grid, position, brightness + 2)
+    end)
+  end
+end
+
+answer = input
+  |> String.split("\n")
+  |> Enum.reduce(%{}, &LightController.perform_operation/2)
+  |> Map.values
+  |> Enum.sum
+
+IO.puts(answer)
