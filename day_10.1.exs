266a0ad6d79da766f9560f40c574107865c9f81a
Bryan Yap <bryan.yap@alliants.com>
2015-12-21 23:07:10 +0000

    day 10 part 1

---
+++ day_10.1.exs
@@ -0,0 +1,37 @@
+input = Interface.read_input
+
+defmodule LookAndSaySequence do
+  def apply(sequence) do
+    build_sequence(sequence)
+  end
+
+  defp build_sequence(sequence) do
+    build_sequence(sequence, [])
+  end
+
+  defp build_sequence("", list_so_far) do
+    list_so_far |> Enum.reverse |> Enum.join("")
+  end
+
+  defp build_sequence(<<char>> <> rest_of_sequence, list_so_far) do
+    {count, remaining_sequence} = count_matches(char, rest_of_sequence, 1)
+
+    build_sequence(remaining_sequence, [<<char>>, to_string(count) | list_so_far])
+  end
+
+  defp count_matches(char, <<char>> <> rest_of_sequence, count_so_far) do
+    count_matches(char, rest_of_sequence, count_so_far + 1)
+  end
+
+  defp count_matches(_char, rest_of_sequence, count_so_far) do
+    {count_so_far, rest_of_sequence}
+  end
+end
+
+answer = 1..40
+  |> Enum.reduce(input, fn(_index, sequence) ->
+    LookAndSaySequence.apply(sequence)
+  end)
+  |> String.length
+
+Interface.print_output(answer)
