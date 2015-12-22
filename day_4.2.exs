63b3393420b727596957af62fd1ae14b81a75e8c
Bryan Yap <bryan.yap@alliants.com>
2015-12-22 14:21:49 +0000

    optimised 4.2

--- day_4.2.exs
+++ day_4.2.exs
@@ -1,29 +1,7 @@
 input = Interface.read_input
 
-defmodule InfiniteSequence do
-  use GenServer
-
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
-  end
-
-  def next_number do
-    GenServer.call(__MODULE__, :next_number)
-  end
-
-  def init(:ok) do
-    {:ok, 1}
-  end
-
-  def handle_call(:next_number, _from, number) do
-    {:reply, number, number + 1}
-  end
-end
-
-{:ok, _pid} = InfiniteSequence.start_link
-
 check_md5 = fn
-  ("000000" <> _rest) ->
+  (<<0, 0, 0>> <> _rest) ->
     true
 
   (_anything_else) ->
@@ -31,13 +9,14 @@ check_md5 = fn
 end
 
 compute_md5 = fn(string) ->
-  :crypto.hash(:md5, input <> string) |> Base.encode16(case: :lower)
+  :crypto.hash(:md5, input <> string)
 end
 
 check_value = fn(value) ->
   value |> to_string |> compute_md5.() |> check_md5.()
 end
 
-answer = Stream.repeatedly(&InfiniteSequence.next_number/0) |> Enum.find(&check_value.(&1))
+answer = Stream.unfold(0, fn(acc) -> {acc, acc + 1} end)
+  |> Enum.find(&check_value.(&1))
 
 Interface.print_output(answer)
